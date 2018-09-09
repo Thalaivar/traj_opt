1cc89b9a7834c64253e5a5f640d70c95979c1d17
Thalaivar <dhruvlaad@gmail.com>
2018-09-09 17:41:50 +0530

    stable orbit

--- NLP/unstable_orbit/unstable_orbit.m
+++ NLP/unstable_orbit/unstable_orbit.m
@@ -1,23 +1,43 @@
 N = 100;
 
 y0 = [0.1;0.1];
-tspan = [0, 3];
+tspan = [0, 2];
 [D, cheb_x] = cheb_diff(N);
-cheb_t = ((tspan(2)-tspan(1))/2)*cheb_x + (sum(tspan))/2;
+cheb_t = flip(((tspan(2)-tspan(1))/2)*cheb_x + (sum(tspan))/2);
 
-[t, ode_samples] = ode45(@system, tspan, y0);
+[t, state] = ode45(@system, cheb_t, y0);
 
-r = ode_samples(:,1); theta = ode_samples(:,2);
-x = zeros(size(r)); y = zeros(size(r));
-for i = 1:length(r)
-   x(i,1) = r(i, 1)*cos(theta(i, 1)); y(i,1) = r(i, 1)*sin(theta(i, 1));
+%r = state(:,1); theta = state(:,2);
+%x = zeros(size(r)); y = zeros(size(r));
+%for i = 1:length(r)
+%   x(i,1) = r(i, 1)*cos(theta(i, 1)); y(i,1) = r(i, 1)*sin(theta(i, 1));
+%end
+%plot(t, r, '.-b', t, theta, '-r')
+
+ode_samples = state;
+for i = 1:length(ode_samples)
+    theta = ode_samples(i,2);
+    if theta > 2*pi, theta = theta - 2*pi;, end
+    if theta < 0, theta = 2*pi + theta;, end
+    ode_samples(i,2) = theta;
 end
-plot(x, y)
-
 x0 = zeros(2*N + 2 + 1, 1);
 x0(1:N+1, 1) = ode_samples(:,1); x0(N+2:2*N+2, 1) = ode_samples(:,2);
-x0(end,1) = cheb_t(1) - cheb_t(end);
+x0(end,1) = -1*(cheb_t(1) - cheb_t(end));
 lb = ones(2*N + 2 + 1, 1); ub = ones(2*N + 2 + 1, 1);
+lb(1:N+1, 1) = 0*lb(1:N+1, 1); ub(1:N+1, 1) = 2*ub(1:N+1, 1);
+lb(N+2:2*N+2, 1) = 0*lb(N+2:2*N+2, 1); ub(N+2:2*N+2, 1) = 2*pi*ub(N+2:2*N+2, 1);
+lb(end,1) = 2; ub(end,1) = Inf;
+
+options = optimoptions('fmincon', 'Display', 'Iter', 'Algorithm', 'sqp', 'MaxFunctionEvaluations', 200000, 'StepTolerance', 1e-15, 'MaxIterations', 1000);
+[x, fval] = fmincon(@objfun, x0, [], [], [], [], lb, ub, @state_const, options);
+
+r = x(1:N+1,1); theta = x(N+2:2*N+2,1);
+x1 = zeros(size(r)); x2 = zeros(size(r));
+for i = 1:length(r)
+   x1(i,1) = r(i, 1)*cos(theta(i, 1)); x2(i,1) = r(i, 1)*sin(theta(i, 1));
+end
+plot(x1,x2)
 
 function ydot = system(t, y)
     r = y(1,1); theta = y(2,1);
@@ -36,20 +56,21 @@ function [c, ceq] = state_const(x)
     diffmat = [D, zeros(N+1, N+1); zeros(N+1, N+1), D];
     xdot_cap = (2/T)*diffmat*[rk;thetak];
     
-    fx = zeros(N+1,1); fy = zeros(N+1,1);
+    fr = zeros(N+1,1); ftheta = zeros(N+1,1);
     for i = 1:N+1
-        temp = system(0, [xk(i);yk(i)]);
-        fx(i, 1) = temp(1,1); fy(i, 1) = temp(2,1);
+        temp = system(0, [rk(i);thetak(i)]);
+        fr(i, 1) = temp(1,1); ftheta(i, 1) = temp(2,1);
     end
-    xdot = [fx;fy];
+    xdot = [fr;ftheta];
     
     c = [];
     ceq = xdot_cap - xdot;
     
-    ceq(end+1) = x(1)-x(N+1);
-    ceq(end+1) = x(N+2)-x(2*N+2)
+    %ceq(end+1) = x(1)-x(N+1);
+    %ceq(end+1) = x(N+2)-x(2*N+2);
 end
 
 function f =  objfun(x)
-    f = x(end)^2;
+    N = (length(x) - 3)/2;
+    f = 10*(x(N+2,1)-x(2*N+2,1))^2 + 10*(x(1,1)-x(N+1,1))^2 + x(end)^2;
 end
