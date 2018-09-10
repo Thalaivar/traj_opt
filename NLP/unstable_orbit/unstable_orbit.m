7c80824ff85ebb56a66ab0b4bd2e5ce5421fbb23
Thalaivar <dhruvlaad@gmail.com>
2018-09-10 20:04:20 +0530

    stable orbit

--- NLP/unstable_orbit/unstable_orbit.m
+++ NLP/unstable_orbit/unstable_orbit.m
@@ -1,58 +1,36 @@
 N = 100;
 
-y0 = [0.1;0.1];
-tspan = [0, 2];
+y0 = [1; 0];
+tspan = [0, 3];
 [D, cheb_x] = cheb_diff(N);
 cheb_t = flip(((tspan(2)-tspan(1))/2)*cheb_x + (sum(tspan))/2);
 
-[t, state] = ode45(@system, cheb_t, y0);
+[t, state] = ode45(@system, tspan, y0);
 
-%r = state(:,1); theta = state(:,2);
-%x = zeros(size(r)); y = zeros(size(r));
-%for i = 1:length(r)
-%   x(i,1) = r(i, 1)*cos(theta(i, 1)); y(i,1) = r(i, 1)*sin(theta(i, 1));
-%end
-%plot(t, r, '.-b', t, theta, '-r')
-
-ode_samples = state;
-for i = 1:length(ode_samples)
-    theta = ode_samples(i,2);
-    if theta > 2*pi, theta = theta - 2*pi;, end
-    if theta < 0, theta = 2*pi + theta;, end
-    ode_samples(i,2) = theta;
-end
-x0 = zeros(2*N + 2 + 1, 1);
-x0(1:N+1, 1) = ode_samples(:,1); x0(N+2:2*N+2, 1) = ode_samples(:,2);
-x0(end,1) = -1*(cheb_t(1) - cheb_t(end));
-lb = ones(2*N + 2 + 1, 1); ub = ones(2*N + 2 + 1, 1);
-lb(1:N+1, 1) = 0*lb(1:N+1, 1); ub(1:N+1, 1) = 2*ub(1:N+1, 1);
-lb(N+2:2*N+2, 1) = 0*lb(N+2:2*N+2, 1); ub(N+2:2*N+2, 1) = 2*pi*ub(N+2:2*N+2, 1);
-lb(end,1) = 2; ub(end,1) = Inf;
+ode_samples = interp1(t, state, cheb_t);
+xy_samp = get_xy(ode_samples);
+plot(xy_samp(:,1), xy_samp(:,2), '-b','Linewidth', 2.0)
+hold on
+x0 = zeros(2*N+2+1, 1); lb = ones(2*N+2+1, 1); ub = ones(2*N+2+1, 1);
+x0(1:N+1, 1) = ode_samples(:,1); x0(N+2:2*N+2, 1) = ode_samples(:,2); x0(end,1) = cheb_t(end) - cheb_t(1);
+lb(1:N+1, 1) = 0*lb(1:N+1, 1); lb(N+2:2*N+2, 1) = -3*lb(N+2:2*N+2, 1); lb(end,1) = 2;
+ub(1:N+1, 1) = 2*ub(1:N+1, 1); ub(N+2:2*N+2, 1) = 7*ub(N+2:2*N+2, 1); ub(end,1) = 3;
 
 options = optimoptions('fmincon', 'Display', 'Iter', 'Algorithm', 'sqp', 'MaxFunctionEvaluations', 200000, 'StepTolerance', 1e-15, 'MaxIterations', 1000);
 [x, fval] = fmincon(@objfun, x0, [], [], [], [], lb, ub, @state_const, options);
 
-r = x(1:N+1,1); theta = x(N+2:2*N+2,1);
-x1 = zeros(size(r)); x2 = zeros(size(r));
-for i = 1:length(r)
-   x1(i,1) = r(i, 1)*cos(theta(i, 1)); x2(i,1) = r(i, 1)*sin(theta(i, 1));
-end
-plot(x1,x2)
-
-function ydot = system(t, y)
-    r = y(1,1); theta = y(2,1);
-    d = -2.0; nu = -pi; a = -3.0; c = pi; b = 5.0; omega = pi;
-    
-    rdot = d*nu*r + a*(r^3);
-    thetadot = omega + c*nu + b*(r^2);
-    ydot = [rdot; thetadot];
-end
+r = x(1:N+1,1); theta = x(N+2:2*N+2,1); T = x(end,1);
+xy = get_xy([r,theta]);
+plot(xy(:,1), xy(:,2), '.-r')
 
 function [c, ceq] = state_const(x)
     N = (length(x) - 3)/2;
     rk = x(1:N+1, 1); thetak = x(N+2:2*N+2,1); T = x(end,1);
     
-    [D, ~] = cheb_diff(N);
+    [D, cheb_x] = cheb_diff(N);
+    %tspan = [0, 3];
+    %cheb_t = flip(((tspan(2)-tspan(1))/2)*cheb_x + (sum(tspan))/2);
+
     diffmat = [D, zeros(N+1, N+1); zeros(N+1, N+1), D];
     xdot_cap = (2/T)*diffmat*[rk;thetak];
     
@@ -65,12 +43,44 @@ function [c, ceq] = state_const(x)
     
     c = [];
     ceq = xdot_cap - xdot;
+    %[M, index] = min(abs(cheb_t - T));
+    %index = N+1;
+    %ceq(end+1) = x(1,1) - x(index,1);
+    %ceq(end+1) = abs(x(N+2,1) - x(N+1+index,1)) - 2*pi;
     
-    %ceq(end+1) = x(1)-x(N+1);
-    %ceq(end+1) = x(N+2)-x(2*N+2);
+    
 end
 
-function f =  objfun(x)
-    N = (length(x) - 3)/2;
-    f = 10*(x(N+2,1)-x(2*N+2,1))^2 + 10*(x(1,1)-x(N+1,1))^2 + x(end)^2;
+function ydot = system(t, y)
+    r = y(1,1); theta = y(2,1);
+    a = -3; b = 5; c = pi; d = -2; nu = -pi; omega = pi;
+    rdot = d*nu*r + a*r^3;
+    thetadot = omega + c*nu + b*r^2;
+    ydot = [rdot;thetadot];
+end
+
+function theta_c = constrain_theta(theta)
+    theta_c = theta;
+    if theta > 2*pi
+        n = floor(theta/(2*pi));
+        theta_c = theta - n*2*pi;
+    end
+    
+    if theta < 0
+        n = floor(theta/(2*pi));
+        theta_c = -n*2*pi + theta;
+    end
+end
+
+function xy = get_xy(state)
+    r = state(:,1); theta = state(:,2);
+    xy = zeros(size(state));
+    for i = 1:length(r)
+      xy(i,1) = r(i)*cos(theta(i)); xy(i,2) = r(i)*sin(theta(i));
+    end
+end
+
+function f = objfun(x)
+    N = (length(x)-3)/2;
+    f = (x(1,1) - x(N+1,1))^2 + (abs(x(N+2,1) - x(2*N+2,1)) - 2*pi)^2 + x(end)^2;
 end
