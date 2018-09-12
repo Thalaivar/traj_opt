b26ebb3fbbd3f7679353907f76e8e9f951ff9b28
Thalaivar <dhruvlaad@gmail.com>
2018-09-12 05:38:48 +0530

    propagating from random point

--- NLP/unstable_orbit/unstable_orbit.m
+++ NLP/unstable_orbit/unstable_orbit.m
@@ -22,10 +22,10 @@ odeopts = odeset('AbsTol',1e-11,'RelTol',1e-11,'MaxStep',0.001);
 [t, xguess] = ode45(@(t, y) sys_model(t, y, p), cheb_t, y0, odeopts);
 
 % first check for initial guess
-xsamp = xguess(:,1).*cos(xguess(:,2));
-ysamp = xguess(:,1).*sin(xguess(:,2));
-plot(xsamp, ysamp, 'b', 'Linewidth', 2.0 )
-hold on
+%xsamp = xguess(:,1).*cos(xguess(:,2));
+%ysamp = xguess(:,1).*sin(xguess(:,2));
+%plot(xsamp, ysamp, 'b', 'Linewidth', 2.0 )
+%hold on
 
 x0 = [xguess(:,1);xguess(:,2);Tguess];
 lb = ones(size(x0)); ub = Inf*ones(size(x0)); 
@@ -47,8 +47,27 @@ fprintf('Time period = %.2f\n',T);
 
 xpos = x(1:N).*cos(x(N+1:2*N));
 ypos = x(1:N).*sin(x(N+1:2*N));
-plot(xpos,ypos,'.-r');
+plot(xpos,ypos,'b', 'LineWidth', 2.0);
 xlabel('x');
 ylabel('y');
 axis equal
 grid on
+hold on
+
+r = x(1:N,1); theta = x(N+1:2*N,1);
+index = floor(rand*100);
+y0 = [r(index);theta(index)];
+tspan = [0, 10];
+odeopts = odeset('AbsTol',1e-11,'RelTol',1e-11,'MaxStep',0.001);
+[t, x_prop] = ode45(@(t, y) sys_model(t, y, p), tspan, y0, odeopts);
+
+xpos1 = x_prop(:,1).*cos(x_prop(:,2));
+ypos1 = x_prop(:,1).*sin(x_prop(:,2));
+plot(xpos1,ypos1,'.-r');
+plot(y0(1,1)*cos(y0(2,1)), y0(1,1)*sin(y0(2,1)), 'sm')
+xlabel('x');
+ylabel('y');
+axis equal
+grid on
+
+
