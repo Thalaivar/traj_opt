29c25d8fcc1a0718905c0bbc71035c64384ff7a7
Thalaivar <dhruvlaad@gmail.com>
2018-09-19 01:03:06 +0530

    brute force

--- NLP/equilibrium/constFun.m
+++ NLP/equilibrium/constFun.m
@@ -1,10 +1,14 @@
 function [c, ceq] = constFun(x, J)
     N = size(J); N = N(1);
-    eig_val = x(1:N,1); eigvec_comp = x(N+1:end, 1);
-    eig_vec = gen_vec(eigvec_comp, N);
+    eig_val = x(1:2*N,1);
+    eigvec_comp = x(2*N+1:end, 1);
+    eig_vec = gen_vec(eigvec_comp, N)
     for i = 1:N
         %ceq(i) = det(J - eig_val(i)*eye(N));
-        ceq(i) = norm(eig_vec(:,i)) - 1;
+        ceq(i,1) = norm(eig_vec(:,i)) - 1;
     end        
+    for i = 1:2:2*N
+        ceq(N+1+i:N+1+i+1,1) = (J - complex(eig_val(i,1), eig_val(i+1,1))*eye(N))*eig_vec(:,mod(i,2))
+    end
     c = [];
 end
--- NLP/equilibrium/eig_objfun.m
+++ NLP/equilibrium/eig_objfun.m
@@ -1,12 +1,12 @@
 function f = eig_objfun(x, J, ch)
     N = size(J); N = N(1);
-    eig_val = x(1:N,1);
-    eigvec_comp = x(N+1:end,1); % components of eigenvectors
+    eig_val = x(1:2*N,1);
+    eigvec_comp = x(2*N+1:end,1); % components of eigenvectors
     eig_vec = gen_vec(eigvec_comp, N); % eig_vals = [v1, v2, ..., vn]
     f = 0;
-    for i = 1:length(eig_val)
-        v1 = (J - eig_val(i,1)*eye(N))*eig_vec(:,i);
-        v2 = det(J - eig_val(i)*eye(N));
-        f = f + v1'*v1 + v2^2;
+    for i = 1:2:N
+        %v1 = (J - complex(eig_val(i,1), eig_val(i+1,1))*eye(N))*eig_vec(:,i);
+        v2 = det(J - complex(eig_val(i), eig_val(i+1,1))*eye(N));
+        f = f + v2*conj(v2);
     end
 end
